a8a9374f175266a9530653603e97811bab858c3b
André Despouys Pascual <andredespouys@gmail.com>
2024-02-04 12:02:13 +0100

    Fix: images path for production

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -64,13 +64,13 @@ function showModal() {
 					<router-link to="/"
 						><img
 							v-if="theme === 'light'"
-							src="@/assets/images/logo-dark.svg"
+							src="/images/logo-dark.svg"
 							class="w-36"
 							alt="Dark Logo"
 						/>
 						<img
 							v-else
-							src="@/assets/images/logo-light.svg"
+							src="/images/logo-light.svg"
 							class="w-36"
 							alt="Light Logo"
 						/>
